7bebe29427ce50121df397d77ca84db0c31f2084
david04davidgr <davidgarcia.r.04@gmail.com>
2026-06-01 13:10:34 +0200

    fix import compression

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import compression from 'compression';
 import helmet from 'helmet';
+import compression = require('compression');
 import { AppModule } from './app.module';
 
 async function bootstrap() {
